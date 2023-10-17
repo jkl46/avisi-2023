1bd1090c1ad1e4136782b08e65229e32bf59c0bf
jkl46 <jay-klein@outlook.com>
2023-10-17 10:40:00 +0200

    gekleurde-vakjes insert

---
+++ gekleurde-vakjes/main.py
@@ -0,0 +1,79 @@
+from solver import GRID_WIDTH, Solver, fieldType, statusTypes
+import requests
+import pygame
+
+TEXTSPACING = 40
+
+BLOCKS = 10
+WIDTH = HEIGHT = GRID_WIDTH * BLOCKS
+clock = pygame.time.Clock()
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 30
+POSTTEXTMARGIN = 10
+
+WIDTH = HEIGHT = GRID_WIDTH * BLOCKS
+
+COLORS = {'BLACK': (0,0,0),
+	'WHITE': (255,255,255),
+	'RED':(255,0,0),
+	'GREEN':(0,255,0),
+	'BLUE':(0,0,255),
+	'GREY': (10,10,10),
+	'ELECTRIFIED': (255,255, 0),
+	'COLD': (102,255,255),
+	'HOT' : (255,65,0),
+	'WET' : (141,255,141),
+	'BACKGROUND' : (100,100,100),
+	}
+pygame.font.init()
+font = pygame.font.SysFont('Arial', 30)
+endPosScreen = font.render('Eind x: 0, y: 49', True, COLORS['BLACK'])
+def drawOverlay(win, solver): 
+	asd = lambda x : "True" if x else "False"
+	for idx, s in enumerate(statusTypes):
+		win.blit(font.render(F"{s} = {asd(solver.statuses[s])}", True, COLORS[s]), (0, idx*TEXTSPACING))
+
+	posScreen = font.render(f"Positie x:{solver.cPos[0]}, y: {solver.cPos[1]}", True, COLORS['BLACK'])
+	win.blit(posScreen, (WIDTH - posScreen.get_width() - POSTTEXTMARGIN, 0))
+	win.blit(endPosScreen, (WIDTH - endPosScreen.get_width() - POSTTEXTMARGIN, posScreen.get_height() + POSTTEXTMARGIN))
+
+
+def drawScreen(win, solver):
+	win.fill(COLORS['BACKGROUND'])
+	for y, row in enumerate(solver.grid):
+		for x, cell in enumerate(row):
+			if cell == 0:
+				continue
+
+			if cell == fieldType['PATH']:
+				c = COLORS['GREEN']
+			elif cell == fieldType['MOUNTAIN']:
+				c = COLORS['BLUE']
+			else:
+				c = COLORS['BLACK']
+			pygame.draw.rect(win, c, (x*BLOCKS, y*BLOCKS, BLOCKS, BLOCKS))
+
+	pygame.draw.rect(win, COLORS['RED'], (BLOCKS*solver.cPos[0], BLOCKS*solver.cPos[1], BLOCKS, BLOCKS))
+	drawOverlay(win, solver)
+
+if __name__ == "__main__":
+	solver = Solver()
+	solver.setSurrounding()
+	running = True
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					running = False
+				elif event.key in [pygame.K_UP, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_LEFT]:
+					solver.walk(event.key)
+					solver.setSurrounding()
+
+		drawScreen(win, solver)
+		pygame.display.update()
+		clock.tick(FPS)
+
+
+
---
+++ gekleurde-vakjes/solver.py
@@ -0,0 +1,79 @@
+import pygame
+import requests
+import json
+
+GRID_WIDTH = GRID_HEIGHT = 100
+
+url = "https://cc-gekleurde-vakjes-4xdpz6dd6q-ez.a.run.app/api"
+
+fieldType = {'UNKNOWN': 0, 'PATH':1, 'MOUNTAIN':2, 'FINISH': 3, 'ELECTRIC' : 4, 'WATER' : 5, 'FIRE' : 6, 'ICE' : 7} #kut python heb geen enums
+statusTypes = ['ELECTRIFIED', 'COLD', 'HOT', 'WET']
+startPos = (25, 49)
+endPos = (0, 49)
+class Solver:
+	def __init__(self):
+		self.finished = False
+		self.grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+		self.path = [startPos]
+		self.cPos = startPos
+		self.statuses = { d: False for d in statusTypes}
+
+		self.grid[self.cPos[1]][self.cPos[0]] = fieldType['PATH']
+		self.grid[endPos[1]][endPos[0]] = fieldType['FINISH']
+
+	def setSurrounding(self):
+		x = self.cPos[0]
+		y = self.cPos[1]
+		d = "UP"
+		for d in ["UP", "DOWN", "LEFT", "RIGHT"]:
+			res = requests.get(f"{url}/look?currentX={x}&currentY={y}&direction={d}")
+			if (res.status_code == 400):
+				continue
+
+			data = json.loads(res.text)
+			try:
+				c = fieldType[data['type']]
+			except Exception:
+				print(f"FOUND {data['type']} at ({data['x']},{data['y']})")
+				c = fieldType['UNKNOWN']
+
+			self.grid[data['y']][data['x']] = c
+
+	def walk(self, key): # Walk and update statuses, (walk resonse text)
+		newPos = ()
+		match key:
+			case pygame.K_UP:
+				newPos = (self.cPos[0], self.cPos[1]+1)
+			case pygame.K_DOWN:
+				newPos = (self.cPos[0], self.cPos[1]-1)
+			case pygame.K_LEFT:
+				newPos = (self.cPos[0]-1, self.cPos[1])
+			case pygame.K_RIGHT:
+				newPos = (self.cPos[0]+1, self.cPos[1])
+
+		payload = '['
+		for p in self.path:
+			payload += '{"x":%d, "y":%d},' % (p[0],p[1])
+		payload += '{"x":%d, "y":%d}' % (newPos[0],newPos[1])
+		payload += ']'
+
+		r = requests.post(f"{url}/walk", json = json.loads(payload))
+
+		if r.status_code != 200:
+			# print(r.text)
+			print("Unable to walk in direction, retry!")
+		else:
+			print("successfully walked!")
+			foo = json.loads(r.text)
+			self.cPos = newPos
+			self.path.append(newPos)
+
+			for s in ['ELECTRIFIED', 'COLD', 'HOT', 'WET']: # update statuses
+				if foo[s] != self.statuses[s]:
+					self.statuses[s] = foo[s]
+
+
+
+if __name__ == "__main__": 
+	s = Solver()
+	s.setSurrounding()
